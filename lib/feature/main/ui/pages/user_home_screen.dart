c10b71068e53f7f5c4e9d528d91869ab92e3dfc6
futsuhai <mmishkinnnn@mail.ru>
2023-04-18 19:14:54 +0300

    add refactored user_home_screen()

--- lib/feature/main/ui/pages/user_home_screen.dart
+++ lib/feature/main/ui/pages/user_home_screen.dart
@@ -1,38 +1,124 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import '../../../../app/domain/error_entity/error_entity.dart';
+import '../../../../app/ui/app_loader.dart';
+import '../../../../app/ui/components/app_snackBar.dart';
 import '../../../auth/domain/auth_state/auth_cubit.dart';
 import '../../../auth/domain/entities/user_entity/user_entity.dart';
+import '../components/app_post_field.dart';
 
 class UserHomeScreen extends StatelessWidget {
-  const UserHomeScreen({Key? key, required this.userEntity}) : super(key: key);
+  UserHomeScreen({Key? key, required this.userEntity}) : super(key: key);
   final UserEntity userEntity;
+
+  final controllerNewPost = TextEditingController();
+
+  //test posts
+  List<String> posts = [
+    "lib/assets/post1.png",
+    "lib/assets/post2.png",
+    "lib/assets/post3.png",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("UserHomeScreen"),
-        actions: [
-          IconButton(
-              onPressed: () => context.read<AuthCubit>().getProfile(),
-              icon: const Icon(Icons.refresh)),
-          IconButton(
-              onPressed: () => context.read<AuthCubit>().logOut(),
-              icon: const Icon(Icons.exit_to_app))
-        ],
-      ),
-      body: Column(
-        children: [
-          Text("username: ${userEntity.username}"),
-          const SizedBox(height: 20),
-          Text("accessToken: ${userEntity.accessToken}"),
-          const SizedBox(height: 20),
-          Text("refreshToken: ${userEntity.refreshToken}"),
-          const SizedBox(height: 20),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("News"),
+          backgroundColor: Colors.grey[900],
+          actions: [
+            IconButton(
+                onPressed: () => context.read<AuthCubit>().logOut(),
+                icon: const Icon(Icons.exit_to_app))
+          ],
+        ),
+        body: BlocConsumer<AuthCubit, AuthState>(listener: (context, state) {
+          state.whenOrNull(authorized: (userEntity) {
+            if (userEntity.userState?.hasData == true) {
+              AppSnackBar.showSnackBarWithMessage(
+                  context, userEntity.userState?.data);
+            }
+            if (userEntity.userState?.hasError == true) {
+              AppSnackBar.showSnackBarWithError(context,
+                  ErrorEntity.fromException(userEntity.userState?.error));
+            }
+          });
+        }, builder: (context, state) {
+          final userEntity = state.whenOrNull(
+            authorized: (userEntity) => userEntity,
+          );
+          if (userEntity?.userState?.connectionState ==
+              ConnectionState.waiting) {
+            return const AppLoader();
+          }
+          return Column(
+            children: [
+              const SizedBox(height: 12),
+              Row(
+                children: [
+                  Expanded(
+                    child: AppPostField(
+                      controller: controllerNewPost,
+                      labelText: "What's new with you?",
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.grey[700],
+                    ),
+                    iconSize: 30.0,
+                    padding: const EdgeInsets.all(4.0),
+                    color: Theme.of(context).colorScheme.background,
+                    splashRadius: 10.0,
+                    constraints: const BoxConstraints(
+                      maxHeight: 40.0,
+                      maxWidth: 40.0,
+                    ),
+                  ),
+                ],
+              ),
+              // Post space
+              const Divider(),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) => Column(
+                          children: [
+                            //Header post
+                            Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                        AssetImage('lib/icons/avatar_test.png'),
+                                  ),
+                                ),
+                                Text(
+                                  userEntity?.username ?? "null",
+                                ),
+                                const Spacer(),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.more_vert))
+                              ],
+                            ),
+                            //Image container
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: Image.asset(
+                                posts[index],
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ],
+                        )),
+              )
+            ],
+          );
+        }));
   }
 }
